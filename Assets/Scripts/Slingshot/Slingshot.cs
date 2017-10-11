1a5ce990aebd176e90040440b67a512a87672dcb
Covina <jabs303@gmail.com>
2017-10-11 08:53:44 -0700

    Section 6, Lecture 60:  Calculate and display LineRenderers, Trajectory Lines; Throw functions

--- Assets/Scripts/Slingshot/Slingshot.cs
+++ Assets/Scripts/Slingshot/Slingshot.cs
@@ -52,7 +52,7 @@ public class Slingshot : MonoBehaviour {
         slingShotState = SlingShotState.Idle;
 
 
-        // Set the line renderer positions
+        // Set the first line renderer positions (vertex)
         slingShotLineRenderer1.SetPosition(0, slingShotLeftOrigin.position);    // left
         slingShotLineRenderer2.SetPosition(0, slingShotRightOrigin.position);   // right
 
@@ -73,7 +73,130 @@ public class Slingshot : MonoBehaviour {
     /// </summary>
     private void InitializeBird()
     {
+        // move bid to staging area
+        birdToThrow.transform.position = birdWaitPosition.position;
+
+        // Put sling state into idle
+        slingShotState = SlingShotState.Idle;
+
+        // Enable the line renderers in prep for shot
+        SetSlingshotLineRenderersActive(true);
+
 
     }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="active"></param>
+    private void SetSlingshotLineRenderersActive(bool active)
+    {
+        // set to true/false
+        slingShotLineRenderer1.enabled = active;
+        slingShotLineRenderer2.enabled = active;
+
+
+    }
+
+
+    /// <summary>
+    /// Set the vertex for the line renderer
+    /// </summary>
+    private void DisplaySlingshotLineRenderers()
+    {
+        // Set the second (index 1) vertex for the waiting state
+        slingShotLineRenderer1.SetPosition(1, birdToThrow.transform.position);
+
+        slingShotLineRenderer2.SetPosition(1, birdToThrow.transform.position);
+
+    }
+
+    /// <summary>
+    /// Set enabled status for trajectory line renderer
+    /// </summary>
+    /// <param name="active"></param>
+    private void SetTrajectoryLineRendererActive(bool active)
+    {
+        slingShotTrajectoryLineRenderer.enabled = active;
+    }
+
+    /// <summary>
+    /// Show the trajectory line on screen
+    /// </summary>
+    /// <param name="distance"></param>
+    private void DisplayTrajectoryLineRenderer(float distance)
+    {
+        // Enable it
+        SetTrajectoryLineRendererActive(true);
+
+        // Calculate and display
+
+        // velocity vector:  v3 distance between bird stage and its current position (player pulling slingshot)
+        Vector3 vel2 = slingShotMiddleVector - birdToThrow.transform.position;
+
+        int segmentCount = 15;
+
+        // create array of vector2 coords
+        Vector2[] segments = new Vector2[segmentCount];
+
+        // set first element as starting loc for the bird to throw
+        segments[0] = birdToThrow.transform.position;
+
+        // how far the bird will go
+        Vector2 segVelocity = new Vector2(vel2.x, vel2.y) * throwSpeed * distance;
+
+        // loop through to fill segments, skipping [0] since we defined that above
+        for(int i = 1; i < segmentCount; i++)
+        {
+            // calculate time between trajectory points
+            float time = i * Time.fixedDeltaTime * 5f;
+
+            // take starting location, then add to it the Distance physics formula:  (v * t ) + (1/2 at^2)
+            segments[i] = segments[0] + (segVelocity * time) + (0.5f * Physics2D.gravity * Mathf.Pow(time, 2) ) ;
+
+        }
+
+        // Set number of vertex positions (using latest docs)
+        slingShotTrajectoryLineRenderer.positionCount = segmentCount;
+
+
+        // populate the vertex positions along the calculated trajectory path
+        for (int i = 0; i < segmentCount; i++) {
+
+            // add the vertex position to the line renderer
+            slingShotTrajectoryLineRenderer.SetPosition(i, segments[i]);
+
+        }
+
+
+    }
+
+
+    /// <summary>
+    /// Throw bird, passing in the distance
+    /// </summary>
+    /// <param name="distance"></param>
+    private void ThrowBird(float distance)
+    {
+
+        // calculate launch velocity
+        Vector3 throwVelocity = slingShotMiddleVector - birdToThrow.transform.position;
+
+        // Prep the bird for throwing
+        birdToThrow.GetComponent<Bird>().OnThrow();
+
+        // Apply the force to the rigidbody and throw the bird
+        birdToThrow.GetComponent<Rigidbody2D>().velocity = new Vector2(throwVelocity.x, throwVelocity.y) * throwSpeed * distance;
+
+        // fire the event for all subscribed delegates
+        if(birdThrown != null)
+        {
+            birdThrown();
+        }
+
+
+    }
+
+
 
 } // Slingshot
