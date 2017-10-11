a7798b44da80b8f4e0eeb87a152246a606e267df
Covina <jabs303@gmail.com>
2017-10-11 00:13:43 -0700

    Section 6, Lecture 59:  Starting script prep for the Slingshot

--- Assets/Scripts/Camera/CameraFollow.cs
+++ Assets/Scripts/Camera/CameraFollow.cs
@@ -4,13 +4,58 @@ using UnityEngine;
 
 public class CameraFollow : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
+    [HideInInspector]
+    public Vector3 startingPosition;
+
+    [HideInInspector]
+    public bool isFollowing;
+
+    // Which bird are we following?
+    [HideInInspector]
+    public Transform birdToFollow;
+
+    // Min camera X position (the start)
+    private float minCameraX = 0.0f;
+
+    // Max camera position (the end)
+    private float maxCameraX = 14f;
+
+
+
+
+	// 
+	void Awake () {
+
+        // get camera starting position
+        startingPosition = transform.position;
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
-	}
+        // are we in the following state?
+        if(isFollowing == true)
+        {
+            // do we have a bird to follow
+            if(birdToFollow != null)
+            {
+                // get bird position
+                var birdPosition = birdToFollow.position;
+
+                // Clamp the X pos value to make sure bird position can't take camera beyond its bounds
+                float x = Mathf.Clamp(birdPosition.x, minCameraX, maxCameraX);
+
+                // Move camera
+                transform.position = new Vector3(x, startingPosition.y, startingPosition.z);
+            }
+            else
+            {
+                // if the bird no longer exists (destroyed), stop following
+                isFollowing = false;
+            }
+
+        }
+
+	} // Update()
 }
---
+++ Assets/Scripts/Slingshot/Slingshot.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Slingshot : MonoBehaviour {
+
+
+    private Vector3 slingShotMiddleVector;
+
+    [HideInInspector]
+    public SlingShotState slingShotState;
+
+
+    // The tops of the two arms of the slingshot
+    public Transform slingShotLeftOrigin, slingShotRightOrigin;
+
+
+    //
+    public LineRenderer slingShotLineRenderer1;
+    public LineRenderer slingShotLineRenderer2;
+    public LineRenderer slingShotTrajectoryLineRenderer;
+
+    // Bird to throw
+    [HideInInspector]
+    public GameObject birdToThrow;
+
+    // 
+    public Transform birdWaitPosition;
+
+    //
+    public float throwSpeed;
+
+    [HideInInspector]
+    public float timeSinceThrown;
+
+    // 
+    public delegate void BirdThrown();
+
+    //
+    public event BirdThrown birdThrown;
+
+
+    // Use this for initialization
+    void Awake () {
+
+        // Set all the line renderers to the foreground
+        slingShotLineRenderer1.sortingLayerName = "Foreground";
+        slingShotLineRenderer2.sortingLayerName = "Foreground";
+        slingShotTrajectoryLineRenderer.sortingLayerName = "Foreground";
+
+        // set state
+        slingShotState = SlingShotState.Idle;
+
+
+        // Set the line renderer positions
+        slingShotLineRenderer1.SetPosition(0, slingShotLeftOrigin.position);    // left
+        slingShotLineRenderer2.SetPosition(0, slingShotRightOrigin.position);   // right
+
+
+        // Calculate the middle vector between the wishbone
+        slingShotMiddleVector = new Vector3( (slingShotLeftOrigin.position.x + slingShotRightOrigin.position.x) / 2,  (slingShotLeftOrigin.position.y + slingShotRightOrigin.position.y) / 2, 0);
+
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    /// <summary>
+    /// 
+    /// </summary>
+    private void InitializeBird()
+    {
+
+    }
+
+} // Slingshot
